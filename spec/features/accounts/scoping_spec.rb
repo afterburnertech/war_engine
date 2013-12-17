55616241972528f2ee80a9554ee8b8c9c0d9c0d5
afterburnertech <brandon@afterburnertechnologies.com>
2013-12-17 15:12:30 -0500

    Testing Forem Integration p111 MWR (last commit was War_Engine integration)

--- spec/features/accounts/scoping_spec.rb
+++ spec/features/accounts/scoping_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper' 
 require 'war_engine/testing_support/factories/account_factory'
+require 'war_engine/testing_support/authentication_helpers'
 feature "Account scoping" do
+	include WarEngine::TestingSupport::AuthenticationHelpers
 	let!(:account_a) { FactoryGirl.create(:account_with_schema) } 
 	let!(:account_b) { FactoryGirl.create(:account_with_schema) } 
 	before do 
