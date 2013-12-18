b8f95a068f67705489567c3fa2d98c13b0d9a597
afterburnertech <brandon@afterburnertechnologies.com>
2013-12-18 14:57:02 -0500

    MWR 4.4

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -2,3 +2,10 @@
 Apartment.excluded_models = ["WarEngine::Account",
                         "WarEngine::Member",
 						"WarEngine::User"]
+						
+# The pluck method here will fetch all the subdomain values 
+# for the records in subscribem_accounts, and will return 
+# them in an array. This array is then used by apartment:migrate
+# to determine which schemas to migrate. For every subdomain in
+# our application, there’s a corresponding schema						
+Apartment.database_names = lambda{ WarEngine::Account.pluck(:subdomain) }
